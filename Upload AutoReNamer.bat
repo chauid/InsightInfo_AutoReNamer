c2d652d8e182fba87862229e5843ac0445ad3589
chauid <dlfdl28@naver.com>
2022-08-08 01:17:44 +0900

    update 1.38ver
    refer to update log.

--- Upload AutoReNamer.bat
+++ Upload AutoReNamer.bat
@@ -3,10 +3,11 @@ set serverIP=34.168.133.173
 if "%time:~0,1%" equ " " (set hours=0%time:~1,1%) else (set hours=%time:~0,2%)
 set uploadname=%date:~2,2%%date:~5,2%%date:~-2,2%_%hours%%time:~3,2%
 echo compact AutoReNamer (1/2)
-zip AutoRenamer.zip AutoRenamer
+zip -r AutoRenamer.zip AutoRenamer>nul
 echo Upload AutoReNamer ^| ServerIP:%serverIP% (2/2)
 scp AutoRenamer.zip root@%serverIP%:/upload/AutoRenamer%uploadname%.zip
 del AutoRenamer.zip
+echo.
 echo Upload Complete! [Upload File:AutoRenamer%uploadname%.zip]
 pause
 exit
